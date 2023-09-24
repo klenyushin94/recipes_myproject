85c24cf07e67112e9b1e2fdfeb2f7bf79dc5ea27
MikhailKlenyushin <klenyushin94@yandex.ru>
2023-09-24 17:12:41 +0300

    Настроил докеры, докер-композ, на данный момент запускается, но не создается рецепт (нет ингредиентов и тэгов) и пропала статика админки

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
@@ -134,7 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
