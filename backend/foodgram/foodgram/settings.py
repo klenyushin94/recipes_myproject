6f4775abe181ea9b03c1acbd2ba413abd45dad38
MikhailKlenyushin <klenyushin94@yandex.ru>
2023-09-27 18:02:57 +0300

    Исправление косяка

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'pzyz25&dk=$dfwz_e&z(qd^qkik)q05^6!bmvuknt$mor90=mq'
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://klenyushin94.ddns.net/']
 
 
 # Application definition
