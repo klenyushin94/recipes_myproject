5faaae3f10234bbccb9a8024683cfe499143688f
MikhailKlenyushin <klenyushin94@yandex.ru>
2023-09-27 15:05:37 +0300

    Попытка запустить CD/CI

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pzyz25&dk=$dfwz_e&z(qd^qkik)q05^6!bmvuknt$mor90=mq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
