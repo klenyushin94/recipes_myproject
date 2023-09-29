964c0a555aba5e3e3c02dfdd5079f23bb9d743c2
MikhailKlenyushin <klenyushin94@yandex.ru>
2023-09-29 23:01:36 +0300

    Попытка изменить частичный поиск

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,14 +5,14 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
 from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from djoser.views import UserViewSet
 from recipes.models import (FavoriteRecipe, Ingredients, RecipeIngredient,
                             Recipes, ShoppingCartRecipe, Subscriptions, Tags,
                             User)
 from reportlab.pdfbase import pdfmetrics, ttfonts
 from reportlab.pdfgen import canvas
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -24,12 +24,13 @@ from .serializers import (CustomUserCreateSerializer, CustomUserSerializer,
                           ShoppingCartSerializer, SubscriptionsSerializer,
                           TagsSerializer)
 
-# class IngredientFilter(FilterSet):
-#     name = filters.CharFilter(lookup_expr='icontains', field_name='name')
 
-#     class Meta:
-#         model = Ingredients
-#         fields = ['name']
+class IngredientFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains', field_name='name')
+
+    class Meta:
+        model = Ingredients
+        fields = ['name']
 
 
 class RecipeFilter(filters.FilterSet):
@@ -130,19 +131,42 @@ class UserViewSet(UserViewSet):
         )
 
 
-class IngredientsViewSet(viewsets.ReadOnlyModelViewSet):
+class IngredientsViewSet(viewsets.ModelViewSet):
     pagination_class = None
     queryset = Ingredients.objects.all()
     serializer_class = IngredientsSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name']
+    filterset_class = IngredientFilter
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class TagsViewSet(viewsets.ReadOnlyModelViewSet):
+class TagsViewSet(viewsets.ModelViewSet):
     pagination_class = None
     queryset = Tags.objects.all()
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     serializer_class = TagsSerializer
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class RecipesViewSet(viewsets.ModelViewSet):
@@ -150,6 +174,11 @@ class RecipesViewSet(viewsets.ModelViewSet):
     pagination_class = UserPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = RecipeFilter
+
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return [permissions.AllowAny]
+    #     return super().get_permissions()
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
